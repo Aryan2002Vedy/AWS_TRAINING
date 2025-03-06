2ea8b2c1638b375001c4135058da65a733db5a3e
Aryan A <aryan_a@epam.com>
2025-03-06 17:52:20 +0530

    second Authi

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -6,13 +6,21 @@ exports.handler = async (event) => {
     if (method === "GET" && path === "/hello") {
         return {
             statusCode: 200,
-            message: "Hello from Lambda",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                statusCode: 200,
+                message: "Hello from Lambda"
+            })
         };
     }
 
     // Return 400 Bad Request for all other cases
     return {
         statusCode: 400,
-        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        })
     };
 };
