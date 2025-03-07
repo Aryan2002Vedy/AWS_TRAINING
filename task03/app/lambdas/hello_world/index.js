fe3ff46ec9dbf057661d2439d2a0b07883358f40
Aryan A <aryan_a@epam.com>
2025-03-07 18:34:05 +0530

    Third Task

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,16 +1,7 @@
 exports.handler = async (event) => {
-    const path = event?.rawPath || "Unknown";
-    const method = event?.requestContext?.http?.method || "Unknown";
-
-    // Allow only GET /hello
-    if (method === "GET" && path === "/hello") {
-        return {
-            statusCode: 200,
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({
-                statusCode: 200,
-                message: "Hello from Lambda"
-            })
-        };
-    }
+    // TODO implement
+    return {
+        statusCode: 200,
+        "message": "Hello from Lambda"
+    };
 };
